7ca9e4eb264c385d7d6295169acc55825d5d66bc
Freek Kalter <freek@kalteronline.org>
2012-09-16 21:03:19 +0200

    clearer messages for travis-ci

--- script/before_build.sh
+++ script/before_build.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env sh
 
+echo "****** Begin before_build script. *******"
 cpanm Dist::Zilla
 dzil authordeps | cpanm --notest --quiet --mirror http://cpan.mirrors.travis-ci.org 
 dzil build
