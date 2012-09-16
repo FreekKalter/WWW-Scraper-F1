a44ac72a4752160bd5618faf1cd3ad4ed14f0bc3
Freek Kalter <freek@kalteronline.org>
2012-09-16 12:21:47 +0200

    [ci skip] fixed some typos in before_build.sh

--- script/before_build.sh
+++ script/before_build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 cpanm Dist::Zilla
-dzil authordeps | cpanm --no-test --quit --mirror http://cpan.mirrors.travis-ci.org 
+dzil authordeps | cpanm --notest --quiet --mirror http://cpan.mirrors.travis-ci.org 
 dzil build
 cd WWW-Scraper-F1*
-cpanm --quit --installdeps --notest --mirror http://cpan.mirrors.travis-ci.org .
+cpanm --quiet --installdeps --notest --mirror http://cpan.mirrors.travis-ci.org .
