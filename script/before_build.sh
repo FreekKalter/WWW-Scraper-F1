9a2d46d9ea337eaaf196195026c5a53729571a33
Freek Kalter <freek@kalteronline.org>
2013-02-14 10:16:45 +0100

    fix travis build with patched readme-plugin

--- script/before_build.sh
+++ script/before_build.sh
@@ -2,7 +2,8 @@
 
 echo "****** Begin before_build script. *******"
 cpanm Dist::Zilla
-dzil authordeps | cpanm --notest --quiet --mirror http://cpan.mirrors.travis-ci.org 
+dzil authordeps | cpanm --notest --quiet --mirror http://cpan.mirrors.travis-ci.org
 dzil build
 cd WWW-Scraper-F1*
 cpanm --quiet --installdeps --notest --mirror http://cpan.mirrors.travis-ci.org .
+cpanm t/assets/Dist-Zilla-Plugin-ReadmeAnyFromPod-0.2.tar.gz
