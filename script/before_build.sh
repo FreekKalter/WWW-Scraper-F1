8436ec4daa9ffc9cca80a1750b826e72f9144eae
Freek Kalter <freek@kalteronline.org>
2013-11-03 21:16:11 +0100

    dont use travis cpan mirror

--- script/before_build.sh
+++ script/before_build.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env sh
 
 echo "****** Begin before_build script. *******"
-cpanm Dist::Zilla
-dzil authordeps | cpanm --notest --quiet --mirror http://cpan.mirrors.travis-ci.org
+cpanm Dist::Zilla --notest --quiet
+dzil authordeps | cpanm --notest --quiet
 dzil build
 cd WWW-Scraper-F1*
-cpanm --quiet --installdeps --notest --mirror http://cpan.mirrors.travis-ci.org .
+cpanm --quiet --installdeps --notest .
