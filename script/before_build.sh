31502110c5b337a20059685f2408b1021d89d58b
Freek Kalter <freek@kalteronline.org>
2013-11-03 21:01:36 +0100

    working copying of README from build to dist

--- script/before_build.sh
+++ script/before_build.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env sh
 
 echo "****** Begin before_build script. *******"
-cpanm --reinstall --notest --quiet t/assets/Dist-Zilla-Plugin-ReadmeAnyFromPod-0.2.tar.gz
 cpanm Dist::Zilla
 dzil authordeps | cpanm --notest --quiet --mirror http://cpan.mirrors.travis-ci.org
 dzil build
